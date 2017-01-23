afbc79d7a3583f15e1b7f163c5f2fe3b65975b01
Rodrigo Machado <rodrigo@loadsmart.com>
2017-01-23 11:45:10 -0200

    Add tests for output functions
    Use https://github.com/heroku/heroku-buildpack-testrunner for testing.

---
+++ test/lib_output_test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# shellcheck source=/dev/null
+. "${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh"
+
+# shellcheck source=/dev/null
+. "${BUILDPACK_HOME}/lib/output.sh"
+
+
+test_info_should_add_7_spaces_at_beginning_of_string()
+{
+    capture info sample string
+    assertCapturedEquals "       sample string"
+}
